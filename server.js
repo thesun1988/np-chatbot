258a9efe68be859e1513333665e2fcd2fec823b3
Nhat Tran <nhattran@Nhats-MacBook-Air.local>
2023-12-20 21:45:19 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./src/configs/viewEngine";
+import webRoutes from "./src/routes/web";
+
+let app = express();
+
+viewEngine(app);
+
+webRoutes(app);
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log("App is running at port: " + port);
+});
